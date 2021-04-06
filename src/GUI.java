70145460b7cb35c4a14c1a9f1f9e7c3354832351
Joel Frost <joelfrost4@gmail.com>
2021-04-06 13:55:15 +0100

    Removed unnecessary imports

--- src/GUI.java
+++ src/GUI.java
@@ -3,8 +3,6 @@ import javax.swing.filechooser.*;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
